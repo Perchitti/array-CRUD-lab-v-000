af73ea42a5828ca388c7d114825230afb004a83c
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 19:39:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
 dogs = ["Scout", "Bronte", "Sophie", "Tank", "arrays!"]
-dogs.pop("arrays!")
+dogs.pop.to_i("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
