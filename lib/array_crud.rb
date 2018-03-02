f3feb5cfacfe90b3fd53ce7b80d9ef5ea07d476b
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 19:36:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 dogs = ["Scout", "Bronte", "Sophie", "Tank"]
-dogs.push("Woof")
+dogs.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
