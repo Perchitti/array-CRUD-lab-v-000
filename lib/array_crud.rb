b381290d7d829ae71f0c38784331cca1cb85a899
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 19:37:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,11 +17,13 @@ dogs.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-
+dogs = ["Scout", "Bronte", "Sophie", "Tank"]
+dogs.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
-
+dogs = ["Scout", "Bronte", "Sophie", "Tank"]
+dogs.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
